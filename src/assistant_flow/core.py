ea90dad8d2ff36fdd709d591483fa796cf4f571e
Daniel Schneider <schneider@volkskammer.org>
2024-06-28 10:14:13 +0200

    understand assistant streaming

--- src/assistant_flow/core.py
+++ src/assistant_flow/core.py
@@ -51,12 +51,7 @@ class AssistantAPI:
                 "You need to provide OPENAI_ASSISTANT_ID in the environment variables"
             )
 
-    def add_message(self, message):
-        _ = self.client.beta.threads.messages.create(
-            thread_id=self.thread_id,
-            role=message["role"],
-            content=message["content"],
-        )
+
 
     def start(self, question):
         # run handler in a separate thread
@@ -82,38 +77,108 @@ class AssistantAPI:
             planner_raw_output=None
         )
 
+    def process_event(self, event):
+        if event.event == "thread.run.created":
+            span = otel_trace.get_current_span()
+            span.set_attribute("promptflow.assistant.run_id", event.data.id)
+            logging.info(f"thread.run.created: run.id {event.data.id}")
+        elif event.event == "thread.message.in_progress":
+            print("\n**** message in progress ****\n")
+        elif event.event == "thread.message.delta":
+            delta = event.data.delta.content[0]
+            if delta.type == "text":
+                print(delta.text.value, end="", flush=True)
+            elif delta.type == "image_file":
+                file_id = delta.image_file.file_id
+                image_base64 = Image(self.client.files.content(file_id).read()).to_base64(with_type=True)
+                print(f"![]({image_base64[:40]})", end="", flush=True)
+            else:
+                print(delta)
+        elif event.event == "thread.run.step.in_progress":
+            print("\n**** step in progress ****\n")
+        elif event.event == "thread.run.step.delta":
+            step_details = event.data.delta.step_details
+            if step_details.type == "tool_calls":
+                for tool_call in step_details.tool_calls:
+                    if not tool_call.id is None:
+                        # this is the header
+                        print(f"  tool_call:")
+                        if tool_call.type == "function":
+                            print(f"    type: {tool_call.type}")
+                            print(f"    id  : {tool_call.id}")
+                            print(f"    name: {tool_call.function.name}")
+                        elif tool_call.type == "code_interpreter":
+                            print(f"    type: {tool_call.type}")
+                        else:
+                            print(f"    type: {tool_call.type}")
+                    else:
+                        # this is the body
+                        if tool_call.type == "function":
+                            print(tool_call.function.arguments, end="", flush=True)
+                        elif tool_call.type == "code_interpreter":
+                            if tool_call.code_interpreter.input:
+                                print(tool_call.code_interpreter.input, end="", flush=True)
+                            elif tool_call.code_interpreter.outputs:
+                                for output in tool_call.code_interpreter.outputs:
+                                    if output.type == "logs":
+                                        print(f"\n{output.logs}", flush=True)
+                            else:
+                                print(tool_call)
+                        else:
+                            print(tool_call)
+            
+        else:
+            logging.info(f"Streaming event: {event.event}")        
+
     @trace    
     def run(self, question):
-        # Add last message in the thread
-        logging.info("Adding message in the thread")
-        self.add_message(dict(role="user", content=question))
         self.question = question
 
         # Run the thread
-        logging.info("Running the thread")
-        run = self.client.beta.threads.runs.create(
-            thread_id=self.thread_id,
-            assistant_id=self.assistant_id,
-        )
-        logging.info(f"Run status: {run.status}")
+        # logging.info("Running the thread")
+        # run = self.client.beta.threads.runs.create(
+        #     thread_id=self.thread_id,
+        #     assistant_id=self.assistant_id,
+        # )
+        # logging.info(f"Run status: {run.status}")
+        run = None
+        start_time = time.time()
 
         # get current span
         span = otel_trace.get_current_span()
         span.set_attribute("promptflow.assistant.message", self.question)
-        span.set_attribute("promptflow.assistant.run_id", run.id)
         span.set_attribute("promptflow.assistant.thread_id", self.thread_id)
         span.set_attribute("promptflow.assistant.assistant_id", self.assistant_id)
 
-        self.queue.send(f"\nRunning message on Thread: {self.thread_id}\n")
+        logging.info("Submitting the message")
+        _ = self.client.beta.threads.messages.create(
+            thread_id=self.thread_id,
+            role="user",
+            content=question,
+        )
 
-        start_time = time.time()
+        logging.info("Streaming the run")
+        with self.client.beta.threads.runs.stream(
+            thread_id=self.thread_id,
+            assistant_id=self.assistant_id,
+            timeout=self.max_waiting_time,
+        ) as stream:
+            logging.info(f"Stream status: {stream}")
+            for event in stream:
+                self.process_event(event)
+            logging.info(f"done streaming")
+            logging.info(f"Run status: {stream.current_run.status}")
+            run = stream.current_run
+
+        # self.queue.send(f"\nRunning message on Thread: {self.thread_id}\n")
+
 
         # loop until max_waiting_time is reached
         while (time.time() - start_time) < self.max_waiting_time:
             # checks the run regularly
-            run = self.client.beta.threads.runs.retrieve(
-                thread_id=self.thread_id, run_id=run.id
-            )
+            # run = self.client.beta.threads.runs.retrieve(
+            #     thread_id=self.thread_id, run_id=run.id
+            # )
             logging.info(
                 f"Run status: {run.status} (time={int(time.time() - start_time)}s, max_waiting_time={self.max_waiting_time})"
             )
@@ -189,19 +254,34 @@ class AssistantAPI:
                         raise ValueError(f"Unsupported tool call type: {tool_call.type}")
 
                 if tool_call_outputs:
-                    _ = self.client.beta.threads.runs.submit_tool_outputs(
+                    # _ = self.client.beta.threads.runs.submit_tool_outputs(
+                    #     thread_id=self.thread_id,
+                    #     run_id=run.id,
+                    #     tool_outputs=tool_call_outputs,
+                    # )
+                    logging.info("Resuming streaming the run")
+                    with self.client.beta.threads.runs.submit_tool_outputs_stream(
                         thread_id=self.thread_id,
                         run_id=run.id,
                         tool_outputs=tool_call_outputs,
-                    )
+                        timeout=self.max_waiting_time - (time.time() - start_time),
+                    ) as stream:
+                        logging.info(f"Stream status: {stream}")
+                        for event in stream:
+                            self.process_event(event)
+                        run = stream.current_run
+                        logging.info(f"done streaming")
+                        logging.info(f"Run status: {stream.current_run.status}")
+
             elif run.status in ["cancelled", "expired", "failed"]:
                 raise ValueError(f"Run failed with status: {run.status}")
 
             elif run.status in ["in_progress", "queued"]:
+                raise ValueError(f"Status should not occur when streaming: {run.status}")
                 # fun running emoji                
                 # self.queue.send(f"{fun_emojis[int(time.time()) % len(fun_emojis)]}")
-                self.queue.send(".")
-                time.sleep(2)
+                # self.queue.send(".")
+                # time.sleep(2)
 
             else:
                 raise ValueError(f"Unknown run status: {run.status}")
@@ -246,7 +326,30 @@ def trace_tool(tool_call):
             "The assistant has asks for tool execution of {}".format(tool_call["function"]["name"])
     )
 
+from openai import AssistantEventHandler
+from typing_extensions import override
 
+class EventHandler(AssistantEventHandler):    
+  @override
+  def on_text_created(self, text) -> None:
+    print(f"\nassistant > ", end="", flush=True)
+      
+  @override
+  def on_text_delta(self, delta, snapshot):
+    print(delta.value, end="", flush=True)
+      
+  def on_tool_call_created(self, tool_call):
+    print(f"\nassistant > {tool_call.type}\n", flush=True)
+  
+  def on_tool_call_delta(self, delta, snapshot):
+    if delta.type == 'code_interpreter':
+      if delta.code_interpreter.input:
+        print(delta.code_interpreter.input, end="", flush=True)
+      if delta.code_interpreter.outputs:
+        print(f"\n\noutput >", flush=True)
+        for output in delta.code_interpreter.outputs:
+          if output.type == "logs":
+            print(f"\n{output.logs}", flush=True)
 
 import queue
 import json
